919ef01785720e6fa203c6ef72affa4aca91460a
Herick Carvalho <heric.carvalho00@gmail.com>
2026-03-23 10:24:53 -0300

    Add files via upload

---
+++ Exercicio 2.1.kt
@@ -0,0 +1,7 @@
+fun main() {
+//    Escreva um programa em que o usuário informe o seu nome e exiba a mensagem "Olá, [NomeDoUsuario]".
+    print("Digite o seu nome: ")
+    var nome = readln()
+    println("Olá, ${nome}")
+
+}
